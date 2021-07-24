6872dcb9aa8b16fce0f8b92febc0576a88a4ac08
= <=>
2021-07-24 23:43:09 +0200

    Function to render xaringan slides

---
+++ R/render_slides.R
@@ -0,0 +1,19 @@
+slides <- list.files("./slides",pattern = "[[:digit:]]{2}_[[:digit:]]{2}_[[:graph:]]*.Rmd")
+
+# This works only with Edge and Chrome
+render_slides <- function(slide = slides){
+  rmarkdown::render(
+    here::here("slides",slide),
+    output_dir = here::here("slides", "rendered_slides")
+  )
+}
+
+# This works with all browsers
+render_slides <- function(slide = slides){
+  rmarkdown::render(
+    here::here("slides",slide)
+  )
+}
+
+
+render_slides(slide = slides[1])
