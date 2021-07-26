bb0c5718102ef100950096b1907109c366cd3432
= <=>
2021-07-26 22:18:09 +0200

    re-render page

---
+++ docs/R/pipeline.R
@@ -0,0 +1,9 @@
+
+# Render slides -----------------------------------------------------------
+
+source("./R/render_slides.R")
+
+
+# Render site -------------------------------------------------------------
+
+rmarkdown::render_site(encoding = "UTF-8")
