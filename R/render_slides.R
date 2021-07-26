8edf93f488bdcee5173ebc5ee22b50b7de9372ca
= <=>
2021-07-26 19:35:56 +0200

    Fix render slides

--- R/render_slides.R
+++ R/render_slides.R
@@ -1,18 +1,20 @@
 slides <- list.files("./slides",pattern = "[[:digit:]]{2}_[[:digit:]]{2}_[[:graph:]]*.Rmd")
 
-# This works only with Edge and Chrome
-render_slides <- function(slide = slides){
-  rmarkdown::render(
-    here::here("slides",slide),
-    output_dir = here::here("slides", "rendered_slides")
-  )
-}
-
-# This works with all browsers
-render_slides <- function(slide = slides){
-  rmarkdown::render(
-    here::here("slides",slide)
-  )
-}
 # Render all slides
 lapply(slides, function(x) {rmarkdown::render(here::here("slides",x))})
+
+
+# # This works only with Edge and Chrome
+# render_slides <- function(slide = slides){
+#   rmarkdown::render(
+#     here::here("slides",slide),
+#     output_dir = here::here("slides", "rendered_slides")
+#   )
+# }
+#
+# # This works with all browsers
+# render_slides <- function(slide = slides){
+#   rmarkdown::render(
+#     here::here("slides",slide)
+#   )
+# }
