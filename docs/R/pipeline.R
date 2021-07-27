245a25f6df6b330cd4c1da4ade1460577b63b6f7
= <=>
2021-07-27 23:20:21 +0200

    re-render slides

--- docs/R/pipeline.R
+++ docs/R/pipeline.R
@@ -4,11 +4,11 @@ source("./R/render_slides.R")
 
 # Render site -------------------------------------------------------------
 
-#rmarkdown::render_site(encoding = "UTF-8")
+rmarkdown::render_site(encoding = "UTF-8")
 
 # Commit ------------------------------------------------------------------
 
-git2r::add(path = here::here("slides"))
+git2r::add(path = here::here("docs"))
 git2r::commit(message = "re-render slides")
 git2r::push(credentials = git2r::cred_token())
 
