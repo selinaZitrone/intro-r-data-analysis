651f3f20efadf30478a1a1fc4747c2ca78dfea55
= <=>
2021-07-30 22:32:55 +0200

    re-render site

--- docs/R/pipeline.R
+++ docs/R/pipeline.R
@@ -9,6 +9,7 @@ rmarkdown::render_site(encoding = "UTF-8")
 # Commit ------------------------------------------------------------------
 
 git2r::add(path = here::here("docs"))
+
 git2r::commit(message = "re-render site")
 git2r::push(credentials = git2r::cred_token())
 
