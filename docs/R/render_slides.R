d9467285717056980043c854613ce2240eda2712
= <=>
2021-07-31 17:36:48 +0200

    re-render site

--- docs/R/render_slides.R
+++ docs/R/render_slides.R
@@ -1,8 +1,15 @@
-slides <- list.files("./slides",pattern = "[[:digit:]]{2}_[[:digit:]]{2}_[[:graph:]]*.Rmd")
+slides_rmd <- list.files("./slides",pattern = "[[:digit:]]{2}_[[:digit:]]{2}_[[:graph:]]*.Rmd")
 
 # Render all slides
-lapply(slides, function(x) {rmarkdown::render(here::here("slides",x))})
+lapply(slides_rmd, function(x) {rmarkdown::render(here::here("slides",x))})
 
+# Print slides to pdf
+
+# Print slides to pdf -----------------------------------------------------
+slides_html <- list.files("./slides",pattern = "[[:digit:]]{2}_[[:digit:]]{2}_[[:graph:]]*.html")
+
+lapply(slides_html, function(x) {pagedown::chrome_print(here::here("slides",x),
+                                                   format = "pdf")})
 
 # # This works only with Edge and Chrome
 # render_slides <- function(slide = slides){
