7af89f0a83251408f6c66ce6905a1267de09ca20
Selina Baldauf <selina.baldauf@gmx.de>
2023-02-28 10:52:48 +0100

    re-render site

--- docs/R/byod/olympics.R
+++ docs/R/byod/olympics.R
@@ -1,6 +1,40 @@
 olympics <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-27/olympics.csv")
 olympics <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-27/olympics.csv')
 
+# look only at the medal winners
+olympics_medal <- filter(olympics, !is.na(medal))
+
+
+# Age distribution --------------------------------------------------------
+
+# count
+olympics_medal %>%
+  ggplot(aes(x=age)) +
+  geom_histogram()
+
+# frequency
+olympics_medal %>%
+  ggplot(aes(x=age, y = ..density..)) +
+  geom_histogram()
+
+# distinguished by sex and medal
+olympics_medal %>%
+  ggplot(aes(x = age)) +
+  geom_histogram() +
+  facet_grid(sex ~ medal, scales = "free_y")
+
+# make the plot nice
+olympics_medal %>%
+  mutate(medal = factor(medal, levels = c("Gold","Silver", "Bronze"))) %>%
+  ggplot(aes(x = age, fill = medal)) +
+  geom_histogram() +
+  facet_grid(sex ~ medal, scales = "free_y") +
+  scale_fill_manual(values = c("gold", "#BCBCBC", "#E2A863")) +
+  theme_minimal()
+
+
+
+
 olympics %>%
   group_by(age) %>%
   summarize(
@@ -91,111 +125,6 @@ olympics %>%
   ggplot(aes(x=team, y=count))+
   geom_col() +
   coord_flip()+
-  facet_wrap(~sex)
-
-
-# sport with the most medals
-olympics %>%
-  group_by(sport) %>%
-  summarize(
-    medals = n()
-  ) %>%
-  arrange(medals)
-
-# What was the Aeoronautics
-olympics %>%
-  filter(sport == "Aeronautics")
-
-olympics %>%
-  group_by(age) %>%
-  summarize(
-    medals = n()
-  ) %>%
-  ggplot(aes(x = age, y = medals)) +
-  geom_line()
-
-olympics %>%
-  group_by(year) %>%
-  summarize(
-    medals = n()
-  ) %>%
-  ggplot(aes(x = year, y = medals)) +
-  geom_line()
-
-# Do male or female win more medals
-olympics %>%
-  group_by(sex) %>%
-  summarize(
-    medals = n()
-  )
-
-# Did this ratio improve over the years?
-olympics %>%
-  group_by(sex, year) %>%
-  summarize(
-    medals = n()
-  ) %>%
-  ggplot(aes(x = year, y = medals, color = sex)) +
-  geom_line()
-
-# yeah it improved
-olympics %>%
-  group_by(sex, year) %>%
-  summarize(
-    medals = n()
-  ) %>%
-  pivot_wider(names_from = sex, values_from = medals) %>%
-  mutate(ratio_m_f = F / M) %>%
-  ggplot(aes(x = year, y = ratio_m_f)) +
-  geom_point() +
-  geom_line()
-
-
-# number of different sports
-unique(olympics$sport)
-# Sport with at leas 10 medals
-olympics %>%
-  group_by(sport) %>%
-  summarise(
-    count = n()
-  ) %>%
-  ungroup() %>%
-  summarise(
-    mean = mean(count)
-  )
-
-
-# Teams with the most medals
-olympics %>%
-  group_by(team) %>%
-  summarize(
-    count = n()
-  ) %>%
-  arrange(
-    desc(count)
-  ) %>%
-  filter(count > 3000) %>%
-  mutate(team = as.factor(team)) %>%
-  mutate(team = fct_reorder(team, count)) %>%
-  ggplot(aes(x = team, y = count)) +
-  geom_col() +
-  coord_flip()
-
-# Teams with the most medals
-olympics %>%
-  group_by(team, sex) %>%
-  summarize(
-    count = n()
-  ) %>%
-  arrange(
-    desc(count)
-  ) %>%
-  filter(count > 1000) %>%
-  mutate(team = as.factor(team)) %>%
-  mutate(team = fct_reorder(team, count)) %>%
-  ggplot(aes(x = team, y = count)) +
-  geom_col() +
-  coord_flip() +
   facet_wrap(~sex)
 
 
