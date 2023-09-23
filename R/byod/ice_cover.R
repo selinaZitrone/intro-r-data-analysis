3c1f57564bd8de3e07970f1688e73ba1ecfb4eca
Selina Baldauf <selina.baldauf@gmx.de>
2023-09-23 22:32:12 +0200

    BYOD Data analysis

--- R/byod/ice_cover.R
+++ R/byod/ice_cover.R
@@ -18,40 +18,44 @@ ggplot(
 
 # Over the years?
 
-ggplot(ice,
-       aes(x=year, y=ice_duration, color=lakeid))+
-  geom_line()+
+ggplot(
+  ice,
+  aes(x = year, y = ice_duration, color = lakeid)
+) +
+  geom_line() +
   geom_smooth(method = "lm")
 
 # fit a model
 
-ice_lm <- lm(ice_duration~year + lakeid + year:lakeid, data = ice)
+ice_lm <- lm(ice_duration ~ year + lakeid + year:lakeid, data = ice)
 drop1(ice_lm, test = "F")
 # seems to be no interaction
-ice_lm2 <- lm(ice_duration~year + lakeid, data = ice)
+ice_lm2 <- lm(ice_duration ~ year + lakeid, data = ice)
 drop1(ice_lm2, test = "F")
 # o difference between lakes
-ice_lm3 <- lm(ice_duration~year, data = ice)
+ice_lm3 <- lm(ice_duration ~ year, data = ice)
 drop1(ice_lm3, test = "F")
 performance::check_model(ice_lm3) # looks ok-ish
 
 # summarize both lakes for average ice cover
-ice_avg <-ice %>%
+ice_avg <- ice %>%
   drop_na(ice_duration) %>%
   group_by(year) %>%
   summarise(ice_duration = mean(ice_duration)) %>%
   rename(avg_ice_duration = ice_duration)
 
-ggplot(ice_avg, aes(x=year, y=avg_ice_duration))+geom_line()+
+ggplot(ice_avg, aes(x = year, y = avg_ice_duration)) +
+  geom_line() +
   geom_smooth(method = "lm")
 
 # What does this have to do with temperature?
 write_csv(ntl_airtemp, "data/ntl_temp.csv")
+
 temp <- read_csv("data/ntl_temp.csv")
 
-ggplot(temp, aes(x=year, y=ave_air_temp_adjusted))+
-  stat_summary()+
-  geom_smooth(method="lm")
+ggplot(temp, aes(x = year, y = ave_air_temp_adjusted)) +
+  stat_summary() +
+  geom_smooth(method = "lm")
 
 # is there a clear trend?
 lm_temp <- lm(ave_air_temp_adjusted ~ year, temp)
@@ -59,15 +63,17 @@ drop1(lm_temp, test = "F")
 performance::check_model(lm_temp)
 
 # summarize mean annual temperature
-mapt <- temp %>% group_by(year) %>% summarize(temp = mean(ave_air_temp_adjusted, na.rm=TRUE))
+mapt <- temp %>%
+  group_by(year) %>%
+  summarize(temp = mean(ave_air_temp_adjusted, na.rm = TRUE))
 lm_temp_Av <- lm(temp ~ year, mapt)
 drop1(lm_temp_Av, test = "F")
 performance::check_model(lm_temp_Av)
 
 # join the tables
 both <- left_join(ice_avg, mapt, by = "year")
-ggplot(both, aes(x=avg_ice_duration,y=temp))+
-  geom_point()+
+ggplot(both, aes(x = avg_ice_duration, y = temp)) +
+  geom_point() +
   geom_smooth(method = "lm")
 
 # Only for the wintermonths
--- R/byod/olympics.R
+++ R/byod/olympics.R
@@ -1,21 +1,65 @@
+library(tidyverse)
 olympics <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-27/olympics.csv")
-olympics <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-27/olympics.csv')
 
-olympics %>%
-  group_by(age) %>%
+# look only at the medal winners
+olympics_medal <- filter(olympics, !is.na(medal))
+
+# Age distribution --------------------------------------------------------
+
+# count
+olympics_medal %>%
+  ggplot(aes(x=age)) +
+  geom_histogram()
+
+# frequency
+olympics_medal %>%
+  ggplot(aes(x=age, y = ..density..)) +
+  geom_histogram()
+
+# distinguished by sex and medal
+olympics_medal %>%
+  ggplot(aes(x = age)) +
+  geom_histogram() +
+  facet_grid(sex ~ medal, scales = "free_y")
+
+# make the plot nice
+olympics_medal %>%
+  mutate(medal = factor(medal, levels = c("Gold","Silver", "Bronze"))) %>%
+  ggplot(aes(x = age, fill = medal)) +
+  geom_histogram() +
+  facet_grid(sex ~ medal, scales = "free_y") +
+  scale_fill_manual(values = c("gold", "#BCBCBC", "#E2A863")) +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+# Looks like age distribution is the same for the medals
+olympics_medal %>%
+  mutate(medal = factor(medal, levels = c("Gold","Silver", "Bronze"))) %>%
+  ggplot(aes(x = age, fill = medal)) +
+  geom_histogram(alpha = 0.5, position = "identity") +
+  facet_wrap(~sex, scales = "free_y") +
+  scale_fill_manual(values = c("gold", "#BCBCBC", "#E2A863")) +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+olympics_medal %>%
+  mutate(medal = factor(medal, levels = c("Gold","Silver", "Bronze"))) %>%
+  ggplot(aes(x = age, fill = sex)) +
+  geom_histogram(alpha = 0.5, position = "identity") +
+  facet_wrap(~medal, scales = "free_y") +
+  scale_fill_manual(values = c("darkorange","cyan4")) +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+# There seem to be more and more medals both in winter and in summer
+olympics_medal %>%
+  group_by(year, season, medal) %>%
   summarize(
     medals = n()
   ) %>%
-  ggplot(aes(x=age, y=medals))+
-  geom_line()
-
-olympics %>%
-  group_by(year) %>%
-  summarize(
-    medals = n()
-  ) %>%
-  ggplot(aes(x=year, y=medals))+
-  geom_line()
+  ggplot(aes(x=year, y=medals, color = medal))+
+  geom_line() +
+  facet_wrap(~season)
 
 # Do male or female win more medals
 olympics %>%
@@ -25,17 +69,18 @@ olympics %>%
   )
 
 # Did this ratio improve over the years?
-olympics %>%
-  group_by(sex, year) %>%
+olympics_medal %>%
+  group_by(sex, year,season) %>%
   summarize(
     medals = n()
   ) %>%
   ggplot(aes(x= year, y= medals, color = sex)) +
-  geom_line()
+  geom_line() +
+  facet_wrap(~season)
 
 # yeah it improved
-olympics %>%
-  group_by(sex, year) %>%
+olympics_medal %>%
+  group_by(sex, year, season) %>%
   summarize(
     medals = n()
   ) %>%
@@ -43,147 +88,13 @@ olympics %>%
   mutate(ratio_m_f = F/M) %>%
   ggplot(aes(x=year, y=ratio_m_f)) +
   geom_point() +
-  geom_line()
-
-
-# number of different sports
-unique(olympics$sport)
-# Sport with at leas 10 medals
-olympics %>%
-  group_by(sport) %>%
-  summarise(
-    count = n()
-  ) %>%
-  ungroup() %>%
-  summarise(
-    mean = mean(count)
-  )
+  geom_line() +
+  facet_wrap(~season)
 
 
 # Teams with the most medals
-olympics %>%
+olympics_medal %>%
   group_by(team) %>%
-  summarize(
-    count = n()
-  ) %>%
-  arrange(
-    desc(count)
-  ) %>%
-  filter(count > 3000) %>%
-  mutate(team = as.factor(team)) %>%
-  mutate(team = fct_reorder(team, count)) %>%
-  ggplot(aes(x=team, y=count))+
-  geom_col() +
-  coord_flip()
-
-# Teams with the most medals
-olympics %>%
-  group_by(team, sex) %>%
-  summarize(
-    count = n()
-  ) %>%
-  arrange(
-    desc(count)
-  ) %>%
-  filter(count > 1000) %>%
-  mutate(team = as.factor(team)) %>%
-  mutate(team = fct_reorder(team, count)) %>%
-  ggplot(aes(x=team, y=count))+
-  geom_col() +
-  coord_flip()+
-  facet_wrap(~sex)
-
-
-# sport with the most medals
-olympics %>%
-  group_by(sport) %>%
-  summarize(
-    medals = n()
-  ) %>%
-  arrange(medals)
-
-# What was the Aeoronautics
-olympics %>%
-  filter(sport == "Aeronautics")
-
-olympics %>%
-  group_by(age) %>%
-  summarize(
-    medals = n()
-  ) %>%
-  ggplot(aes(x = age, y = medals)) +
-  geom_line()
-
-olympics %>%
-  group_by(year) %>%
-  summarize(
-    medals = n()
-  ) %>%
-  ggplot(aes(x = year, y = medals)) +
-  geom_line()
-
-# Do male or female win more medals
-olympics %>%
-  group_by(sex) %>%
-  summarize(
-    medals = n()
-  )
-
-# Did this ratio improve over the years?
-olympics %>%
-  group_by(sex, year) %>%
-  summarize(
-    medals = n()
-  ) %>%
-  ggplot(aes(x = year, y = medals, color = sex)) +
-  geom_line()
-
-# yeah it improved
-olympics %>%
-  group_by(sex, year) %>%
-  summarize(
-    medals = n()
-  ) %>%
-  pivot_wider(names_from = sex, values_from = medals) %>%
-  mutate(ratio_m_f = F / M) %>%
-  ggplot(aes(x = year, y = ratio_m_f)) +
-  geom_point() +
-  geom_line()
-
-
-# number of different sports
-unique(olympics$sport)
-# Sport with at leas 10 medals
-olympics %>%
-  group_by(sport) %>%
-  summarise(
-    count = n()
-  ) %>%
-  ungroup() %>%
-  summarise(
-    mean = mean(count)
-  )
-
-
-# Teams with the most medals
-olympics %>%
-  group_by(team) %>%
-  summarize(
-    count = n()
-  ) %>%
-  arrange(
-    desc(count)
-  ) %>%
-  filter(count > 3000) %>%
-  mutate(team = as.factor(team)) %>%
-  mutate(team = fct_reorder(team, count)) %>%
-  ggplot(aes(x = team, y = count)) +
-  geom_col() +
-  coord_flip()
-
-# Teams with the most medals
-olympics %>%
-  group_by(team, sex) %>%
   summarize(
     count = n()
   ) %>%
@@ -195,18 +106,77 @@ olympics %>%
   mutate(team = fct_reorder(team, count)) %>%
   ggplot(aes(x = team, y = count)) +
   geom_col() +
-  coord_flip() +
-  facet_wrap(~sex)
+  coord_flip()
+
+# Teams with the most medals evolution off ggplot style
+# https://www.cedricscherer.com/2019/05/17/the-evolution-of-a-ggplot-ep.-1/
+medal_sums_total <-
+  olympics_medal %>%
+  group_by(team) %>%
+  count() %>%
+  arrange(desc(n)) %>%
+  filter(n>1000)
+
+year_count <- olympics_medal %>%
+  filter(team %in% medal_sums_total$team) %>%
+  group_by(team,year) %>%
+  count()
+
+# average medal count by year
+avg <- mean(year_count$n)
+
+year_count %>%
+  mutate(team = fct_reorder(team, -n)) %>%
+  ggplot(aes(x = n, y = team, color = team)) +
+  geom_vline(xintercept = avg, color = "gray70") +
+  geom_point(
+    position = position_jitter(
+      seed = 123,
+      width = 0.2
+    ),
+    alpha = 0.25
+  ) +
+  geom_segment(
+    aes(x = avg, xend = n, y = team, yend = team)
+  )+
+  stat_summary(fun = mean, geom = "point", size = 5) +
+  ggsci::scale_color_uchicago() +
+  labs(x = "Yearly medal count") +
+  theme_light(base_size = 18, base_family = "Poppins") +
+  theme(
+    legend.position = "none",
+    axis.title = element_text(size = 12),
+    axis.text.x = element_text(family = "Roboto Mono", size = 10),
+    plot.caption = element_text(size = 9, color = "gray50"),
+    panel.grid = element_blank()
+  )
+
 
 
 # sport with the most medals
-olympics %>%
+olympics_medal %>%
   group_by(sport) %>%
   summarize(
     medals = n()
   ) %>%
   arrange(medals)
 
-# What was the Aeoronautics
-olympics %>%
-  filter(sport == "Aeronautics")
+
+# Heatmaps -----------------------------------------------------------------
+# heat map showing the number of medals by medal type and sport/country/gender...
+olympics_medal %>%
+  group_by(medal, sex) %>%
+  count() %>%
+  ggplot(aes(x=medal, y=sex, fill = n))+
+  geom_tile() +
+  scale_fill_gradient(low = "#00AFBB", high = "#E7B800")
+
+olympics_medal %>%
+  group_by(medal, noc) %>%
+  count() %>%
+  arrange(desc(n)) %>%
+  filter(n>)
+  ggplot(aes(x=medal, y=sex, fill = n))+
+  geom_tile() +
+  scale_fill_gradient(low = "#00AFBB", high = "#E7B800")
+
