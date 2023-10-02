2ed4d185a639cc84088985878e8e33087a13fec3
Selina Baldauf <selina.baldauf@gmx.de>
2023-10-02 20:15:00 +0200

    Fix solution readr

--- make.R
+++ make.R
@@ -1,2 +1,18 @@
 quarto::quarto_render()
 
+# render slides as pdf
+slides_html <- list.files(here::here("docs/slides"),
+                          pattern = "!00_", full.names = TRUE
+)
+
+lapply(slides_html, function(x) {
+  pagedown::chrome_print(x,
+                         format = "pdf"
+  )
+})
+
+# commit and push
+git2r::add(path = here::here("docs"))
+git2r::commit(message = "re-render site")
+git2r::push(credentials = git2r::cred_token())
+
