54a3000ea937aa0643d95e5613f6067f06f0ee0f
Selina Baldauf <selina.baldauf@gmx.de>
2023-10-13 16:11:44 +0200

    Update Organizational slides

--- make.R
+++ make.R
@@ -6,9 +6,13 @@ slides_html <- list.files(here::here("docs/sessions/slides"),
 )
 
 # does not work
-slides_html <- slides_html[4]
+slides_html <- slides_html[4][c(1:3, 5:8, 10:14)]
 
-lapply(slides_html[5], function(x) {
+# remove non working pdf files: vector index 4
+# slides_html <- slides_html[-c(4)]
+
+
+lapply(slides_html, function(x) {
   print(paste0("Printing ", x))
   pagedown::chrome_print(x,
                          format = "pdf"
