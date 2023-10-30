3a38f84e1b322984082de4fa8f89e9579f9e858e
Selina Baldauf <selina.baldauf@gmx.de>
2023-10-30 14:12:37 +0100

    Final version

--- make.R
+++ make.R
@@ -5,13 +5,6 @@ slides_html <- list.files(here::here("docs/sessions/slides"),
                           pattern = "*.html", full.names = TRUE
 )
 
-# does not work
-slides_html <- slides_html[4][c(1:3, 5:8, 10:14)]
-
-# remove non working pdf files: vector index 4
-# slides_html <- slides_html[-c(4)]
-
-
 lapply(slides_html, function(x) {
   print(paste0("Printing ", x))
   pagedown::chrome_print(x,
