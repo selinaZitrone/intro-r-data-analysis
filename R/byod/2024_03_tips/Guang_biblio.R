a99f907664b144e0eba7b5afe66fbde2887fb24e
Selina Baldauf <selina.baldauf@gmx.de>
2024-09-20 15:40:27 +0200

    Stop tracking byod of 03/24

--- R/byod/2024_03_tips/Guang_biblio.R
+++
@@ -1,3 +0,0 @@
-# https://github.com/yingfei266/bibliometrix-by-R
-# Tutorial: https://www.bibliometrix.org/vignettes/Introduction_to_bibliometrix.html
-install.packages("bibliometrix")
--- R/byod/2024_03_tips/Marco_Nexus.R
+++
@@ -1,3 +0,0 @@
-library(ape)
-
-# https://guangchuangyu.github.io/treedata-workshop/r-treeio.html
