d3305c2963f033f5e8ab844b8810e5f1582943e4
Selina Baldauf <selina.baldauf@fu-berlin.de>
2025-02-28 11:22:48 +0100

    Update random group assignment

--- R/random_groups.R
+++ R/random_groups.R
@@ -1,33 +1,28 @@
 names <- c(
-  "Muhammad-Ali",
-  "Marco",
-  "Yves",
-  "Amina",
-  "Sofia",
-  "Antreas",
-  "Xuemei",
-  "Philipp",
-  "Elif",
-  "Caro",
-  "Max",
-  "Jin",
-  "Matteo",
-  "Guang",
-  "Vera",
-  "Orlando"
+  "Fabian", "Sara",
+  "Vajiheh", "Kinga",
+  "Haicheng", "Xiaohui",
+  "zineb", "Keqing",
+  "Malkeet", "Johannes",
+  "Xuchao", "Desjana",
+  "Henriette ", "Saidaqa ",
+  "Mohammad Reza", "Malavika"
 )
-ngroups <- 4
-groupsize <- 4
 
-assign_groups(ngroups = ngroups, groupsize = groupsize, names = names)
-
-assign_groups <- function(ngroups, groupsize, names) {
+# Function to assign people to groups
+assign_groups <- function(groupsize, names) {
   groups <- list()
+  # How many groups can there be?
+  ngroups <- length(names) %/% groupsize
   for (i in 1:ngroups) {
     groups[[i]] <- sample(names, size = groupsize)
     names <- names[!(names %in% groups[[i]])]
+    # convert groups to a nice string
+    groups[[i]] <- paste(groups[[i]], collapse = ", ")
   }
   names(groups) <- paste0("group_", 1:i)
   groups[["not_assigned_yet"]] <- names
   return(groups)
 }
+
+assign_groups(groupsize = 4, names = names)
