5d9cae8855a8018fdb91e8b7be7a5dc0cd8825a7
Selina Baldauf <selina.baldauf@gmx.de>
2025-10-01 16:12:17 +0200

    Format

--- R/Build_pipeline/pipeline.R
+++ R/Build_pipeline/pipeline.R
@@ -1,19 +1,19 @@
 # render the website
-quarto::quarto_render()
+system("quarto render")
 
 # render slides as pdf
-slides_html <- list.files(here::here("docs/sessions/slides/"),
-  pattern = "*.html", full.names = TRUE
+slides_html <- list.files(
+  here::here("docs/sessions/slides/"),
+  pattern = "*.html",
+  full.names = TRUE
 )
 
 lapply(slides_html, function(x) {
-  pagedown::chrome_print(x,
-    format = "pdf"
-  )
+  pagedown::chrome_print(x, format = "pdf")
   print(paste0("Printed ", x))
 })
 
 # commit and push
 git2r::add(path = here::here("docs"))
 git2r::commit(message = "re-render site")
-git2r::push(credentials = git2r::ssh_path())
+system("git push")
