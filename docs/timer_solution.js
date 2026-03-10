e8db44da9a780b3df7eb2c5e3212f3bb3692be34
Selina Baldauf <selina.baldauf@fu-berlin.de>
2026-03-10 15:02:19 +0100

    Upload day2 solution

--- docs/timer_solution.js
+++ docs/timer_solution.js
@@ -2,7 +2,7 @@
 // Always include the Berlin timezone offset: +02:00 (summer, CEST) or +01:00 (winter, CET)
 const session_end_times = [
   new Date("2026-03-09T16:00+01:00"), // day 1
-  new Date("2026-03-10T16:00+01:00"), // day 2
+  new Date("2026-03-10T15:00+01:00"), // day 2
   new Date("2026-03-16T16:00+01:00"), // day 3
 ];
 
